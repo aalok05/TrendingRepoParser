2533e23ce182ebae6402e0f986854fbc9dce128f
Aalok Singh <aalok_2@live.com>
2018-02-02 14:43:06 +0530

    added method overload for filtering by language

--- TrendingRepoParser.Tests/TrendingRepoParserServiceTests.cs
+++ TrendingRepoParser.Tests/TrendingRepoParserServiceTests.cs
@@ -11,9 +11,26 @@ namespace TrendingRepoParser.Tests
         [InlineData(TimeRange.TODAY)]
         [InlineData(TimeRange.WEEKLY)]
         [InlineData(TimeRange.MONTHLY)]
-        public async Task ReturnsTrendingPages(TimeRange timeRange)
+        public async Task ReturnsTrendingRepos(TimeRange timeRange)
         {
             var trendingPages = await TrendingRepoParserService.ExtractTrendingRepoNames(timeRange);
+
+            trendingPages.Should().NotBeNull();
+            trendingPages.Should().NotBeEmpty();
+            foreach (var trendingPage in trendingPages)
+            {
+                trendingPage.Item1.Should().NotBeNullOrEmpty();
+                trendingPage.Item2.Should().NotBeNullOrEmpty();
+            }
+        }
+
+        [Theory]
+        [InlineData(TimeRange.TODAY, Language.Go)]
+        [InlineData(TimeRange.WEEKLY, Language.CSharp)]
+        [InlineData(TimeRange.MONTHLY, Language.JavaScript)]
+        public async Task ReturnsTrendingReposOfLanguage(TimeRange timeRange, Language language)
+        {
+            var trendingPages = await TrendingRepoParserService.ExtractTrendingRepoNames(timeRange, language);
 
             trendingPages.Should().NotBeNull();
             trendingPages.Should().NotBeEmpty();
--- TrendingRepoParser/TrendingRepoParserService.cs
+++ TrendingRepoParser/TrendingRepoParserService.cs
@@ -50,5 +50,43 @@ namespace TrendingRepoParser
             }
             return repoNames;
         }
+
+        public async static Task<List<Tuple<string, string>>> ExtractTrendingRepoNames(TimeRange range, Language language)
+        {
+            string url = "";
+
+            List<Tuple<string, string>> repoNames = new List<Tuple<string, string>>();
+
+            if (range == TimeRange.TODAY)
+            {
+                url = "https://github.com/trending/" + language.ToString() + "?since=daily";
+            }
+            if (range == TimeRange.WEEKLY)
+            {
+                url = "https://github.com/trending/" + language.ToString() + "?since=weekly";
+            }
+            if (range == TimeRange.MONTHLY)
+            {
+                url = "https://github.com/trending/"+ language.ToString() +"?since=monthly";
+            }
+
+            //
+            // The following code loads the HTML page and looks for specific tags so that we get what we want.
+            // This code will have to be changed as and when the Github Trending page changes its HTML.
+            // We will have to revise the logic as per the new page structure.
+            //
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = await web.LoadFromWebAsync(url);
+            var h3 = doc.DocumentNode.Descendants("h3");
+
+            foreach (var i in h3)
+            {
+                var s = i.Descendants("a").First();
+                var names = s.Attributes["href"].Value.Split('/');
+                repoNames.Add(new Tuple<string, string>(names[1], names[2]));
+            }
+            return repoNames;
+        }
     }
 }
